21236d705e9c88f6e6364709a6255bee026baff9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 10:42:30 +0300

    Added a global padding to the main paragraph tag inside the page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<p className='flex justify-center items-center h-full w-screen'>
+		<p className='flex justify-center items-center h-full w-screen p-5'>
 			Hello
 		</p>
 	)
