282401a46e16e9484d1ca756567dc5dc3aff38cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 10:42:48 +0300

    Changed the paragraph tag to a div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<p className='flex justify-center items-center h-full w-screen p-5'>
+		<div className='flex justify-center items-center h-full w-screen p-5'>
 			Hello
-		</p>
+		</div>
 	)
 }
