0660b70cfddee62ee7c922376c8028b80bbd1ef2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 10:14:09 +0300

    Made the app width fullscreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-	return <p className='flex justify-center items-center h-screen'>Hello</p>
+	return (
+		<p className='flex justify-center items-center h-screen w-screen'>
+			Hello
+		</p>
+	)
 }
