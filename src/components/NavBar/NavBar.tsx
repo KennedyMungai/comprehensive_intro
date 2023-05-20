cbf8e9c369795cc8d04120788d4dd2175e8f14a4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 10:23:32 +0300

    The NavBar Basics

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 type Props = {}
 
 const NavBar = (props: Props) => {
-	return <div>NavBar</div>
+    return (
+		<nav className='bg-gray-800'>
+			<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+				<div className='relative flex h-16 items-center sm:hidden'>
+					<div className='absolute inset-y-0 left-0 flex items-center sm:hidden'></div>
+				</div>
+			</div>
+		</nav>
+	)
 }
 
 export default NavBar
