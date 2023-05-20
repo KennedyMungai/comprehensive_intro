4722fe12741f3a6eee499568dad307a5e0207145
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 10:29:03 +0300

    Changed the bg color of the NavBar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const NavBar = (props: Props) => {
     return (
-		<nav className='bg-gray-800'>
+		<nav className='bg-gray-500'>
 			<div className='h-20 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
 				<div className='relative flex h-16 items-center sm:hidden'>
 					<div className='absolute inset-y-0 left-0 flex items-center sm:hidden'></div>
