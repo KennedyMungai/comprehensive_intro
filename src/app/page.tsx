a0f633f6cab84e880a23298640b995d6444667df
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 10:45:01 +0300

    Added a Link to the about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
 	return (
-		<div className='flex justify-center items-center h-full w-screen p-5'>
+		<div className='flex flex-col justify-center items-center h-full w-screen p-5 gap-3'>
 			Hello
+			<button className='p-5 bg-orange-500 rounded-lg'>
+				<Link href={'/about'}>Link to the About Page</Link>
+			</button>
 		</div>
 	)
 }
