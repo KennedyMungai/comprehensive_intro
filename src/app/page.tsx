c3b033a433653f9a9d92204211639315eeae5e06
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 10:27:27 +0300

    CHanged the height to be full screen to height full available space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<p className='flex justify-center items-center h-screen w-screen'>
+		<p className='flex justify-center items-center h-full w-screen'>
 			Hello
 		</p>
 	)
