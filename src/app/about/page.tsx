9cacc1a9926aa8eca3f66b708708546a2098e70c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-20 10:41:07 +0300

    Updated the test on the Link

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const About = (props: Props) => {
 		<>
 			<div>About</div>
 			<button className='bg-orange-500 rounded-md p-2 m-2'>
-				<Link href='/'>To Home Page</Link>
+				<Link href='/'>Link To Home Page</Link>
 			</button>
 		</>
 	)
